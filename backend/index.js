f32c739de3e788b0bfe052ced1da971f58cd0707
Aditya <itsmeaditya3254@gmail.com>
2025-03-30 19:43:34 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,13 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.get('/',(req,res)=>{
+    res.send({
+        activeStatus:true,
+        error:false,
+    })
+})
+
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on port ${PORT}`);
